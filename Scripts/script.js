78a6bce369ee64a0b5ea28c986c35ff4f3dfcfd6
IanJoseph <Ianjoseph630@gmail.com>
2022-02-07 11:49:46 +0300

    Changed a few styles

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,9 +1,15 @@
 let testimonialsBtn = document.querySelector('#testimonials-btn');
 let ECommerceBtn = document.querySelector('#e-commerce-btn');
+let moviesBtn = document.querySelector('#movies-btn');
 
 testimonialsBtn.addEventListener('click',()=>{
     window.open('https://github.com/IanGathuita/testimonials-grid-section', '_blank');
 });
+
 ECommerceBtn.addEventListener('click',()=>{
     window.open('https://github.com/IanGathuita/ecommerce-product-page', '_blank');
+});
+
+moviesBtn.addEventListener('click',()=>{
+    window.open('https://github.com/IanGathuita/movies', '_blank');
 });
